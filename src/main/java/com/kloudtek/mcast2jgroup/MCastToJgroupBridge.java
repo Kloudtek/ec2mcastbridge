4aaf1903689800ece50603edc603665bf842621e
Yannick Menager <ymenager@kloudtek.com>
2016-07-03 10:32:57 -0700

    migrating to wildfly

---
+++ src/main/java/com/kloudtek/mcast2jgroup/MCastToJgroupBridge.java
@@ -0,0 +1,14 @@
+package com.kloudtek.mcast2jgroup;
+
+import org.jgroups.JChannel;
+
+/**
+ * Created by yannick on 29/6/16.
+ */
+public class MCastToJgroupBridge {
+    private JChannel jChannel;
+    private String clusterName;
+
+    public MCastToJgroupBridge() {
+    }
+}
---
+++ src/test/java/com/kloudtek/mcast2jgroup/MCastToJgroupBridgeTest.java
@@ -0,0 +1,10 @@
+package com.kloudtek.mcast2jgroup;
+
+/**
+ * Created by yannick on 29/6/16.
+ */
+public class MCastToJgroupBridgeTest {
+    public void testBridge() {
+
+    }
+}
